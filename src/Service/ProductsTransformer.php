8962f6e07bdcda03ad706849018a59430f70f71a
emilveldhuizen <emilveldhuizen@gmail.com>
2019-04-17 22:11:09 +0200

    phpdoc for product entity in transformer

--- src/Service/ProductsTransformer.php
+++ src/Service/ProductsTransformer.php
@@ -3,12 +3,14 @@
 namespace App\Service;
 
 use App\DTO\ProductWithUpdateDate;
+use App\Entity\Product;
 
 class ProductsTransformer
 {
     public function transformProductsIntoDto($products, $lastUpdatedVariations): array
     {
         $productsWithUpdateDate = [];
+        /** @var Product $product */
         foreach ($products as $product) {
             $productWithUpdateDate = new ProductWithUpdateDate();
             $productWithUpdateDate->setProduct($product);
